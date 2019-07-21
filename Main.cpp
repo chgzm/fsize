48e53fdef13ad0e17e9378efd6db5a1178648210
chgzm <antnhm54@gmail.com>
2019-07-21 23:40:27 +0900

    fix argument to add help option

--- Main.cpp
+++ Main.cpp
@@ -8,25 +8,26 @@
 static constexpr struct option longopts[] = {
     { "num",     required_argument, nullptr, 'n' },
     { "reverse", no_argument,       nullptr, 'r' },
-    { "help",    no_argument,       nullptr, 'r' },
+    { "help",    no_argument,       nullptr, 'h' },
     { 0,         0,                 0,        0  },
 };
 
 static void usage() noexcept {
     std::printf(
-        "Usage: fsize [-r] [-n num] [directory]\n"
+        "Usage: fsize [-r] [-n num] [-h] [directory]\n"
         " -n, --num=N    Set number of files displayed\n"
         " -r, --reverse  Show files in ascending order\n"
-        "\n"
+        " -h, --help     Show help and exit\n"
     );        
 } 
 
 static int parseCommandLine(int argc, char* argv[], fs::CollectorParam& param) noexcept {
     int opt = 0, longIndex = 0;
-    while ((opt = getopt_long(argc, argv, ":n:r", longopts, &longIndex)) != -1) {
+    while ((opt = getopt_long(argc, argv, ":n:rh", longopts, &longIndex)) != -1) {
         switch (opt) {
-        case 'n': { param.numOfLines   = std::stoi(optarg); break; }
-        case 'r': { param.reverseFlag  = true;              break; }
+        case 'n': { param.numOfLines   = std::stoi(optarg); break;   }
+        case 'r': { param.reverseFlag  = true;              break;   }
+        case 'h': { usage();                                exit(0); }
         default:  { 
             std::fprintf(stderr, "Invalid option '%c'\n", opt);
             return -1;
